855355d161ec6a78b64dd8d9e2b326c796680c70
VIKAS REDDY KALAMALLA <vikasreddyk01649@gmail.com>
2026-07-15 05:06:03 +0530

    Add files via upload

---
+++ Java/Elseifladder.java
@@ -0,0 +1,19 @@
+public class Elseifladder{
+    public static void main(String[] args) {
+        {
+            int age = 18;
+            if(age >18){
+                System.out.println("You are eligible for voting");
+            }
+            else if (age == 18){
+
+            
+                System.out.println("You are also eligible ");
+
+            }
+            else {
+                System.out.println("You are not eligible for voting");
+            }
+        }
+    }
+}
